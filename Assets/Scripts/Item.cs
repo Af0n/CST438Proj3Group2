f64de804cdd70b9fb63ed52035ecd43e8118f4f7
AndAgGit <afon.cyffrous@gmail.com>
2024-11-06 15:53:10 -0800

    Added item sprite changing

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,24 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
 
 public class Item : MonoBehaviour
 {
-    public ItemSprites sprites;
-
+    public ItemSprites itemSprites;
     public ItemType type;
-
     public bool isHeld;
+    public bool debug;
 
-    private void Awake() {
-        // set sprite
+    private SpriteRenderer spriteRenderer;
+    private int typeCode;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody>();
+
+        ChangeItem(type);
     }
 
-    public void PickUp(){
+    private void Update() {
+        if(debug){
+            ChangeItem(type);
+        }
+    }
+
+    // not intended to be used as a pickup script.
+    // does things to the item when picked up
+    public void PickUp()
+    {
         isHeld = true;
+        rb.isKinematic = true;
     }
 
-    public void Drop(){
+    // not intended to be used as a pickup script.
+    // does things to the item when dropped
+    public void Drop()
+    {
         isHeld = false;
+        rb.isKinematic = false;
+    }
+
+    public void SetSprite(Sprite set){
+        spriteRenderer.sprite = set;
+    }
+
+    // handles all variables when changing item to a new type
+    public void ChangeItem(ItemType newType){
+        type = newType;
+        typeCode = GetItemTypeCode();
+        SetSprite(itemSprites.sprites[typeCode]);
+    }
+
+    // gets the numerical value of the item's type
+    // an enum doesnt actually equal a number, the value is used for ordering in Unity editor
+    public int GetItemTypeCode()
+    {
+        switch (type)
+        {
+            case ItemType.BUCKET:
+                return 0;
+            case ItemType.SALTWATER:
+                return 1;
+            case ItemType.FRESH_APPLE:
+                return 2;
+            case ItemType.RAW_RUBY:
+                return 3;
+            case ItemType.DISORDERED_SPIRIT:
+                return 4;
+            case ItemType.SPARKLING_APPLE:
+                return 5;
+            case ItemType.MINOR_SPIRIT:
+                return 6;
+            case ItemType.PURE_WATER:
+                return 7;
+            case ItemType.SALT:
+                return 8;
+            case ItemType.SPARKLING_RUBY:
+                return 9;
+            case ItemType.GEMSTONE_DUST:
+                return 10;
+            case ItemType.POTION_CALM:
+                return 11;
+            case ItemType.POTION_HEAL:
+                return 12;
+            case ItemType.POTION_MANA:
+                return 13;
+            default:
+                Debug.Log("Could Not Find Type");
+                return -1;
+        }
     }
 }

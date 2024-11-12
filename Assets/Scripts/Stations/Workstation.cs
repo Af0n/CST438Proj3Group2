b4c9dc262404a70c0ee51f63d9ebe9a45f8e11cf
afonc <37196107+Af0n@users.noreply.github.com>
2024-11-11 21:46:11 -0800

    Adding tick simulating to the stations
    - also renamed some things in ResourceNodes

--- Assets/Scripts/Stations/Workstation.cs
+++ Assets/Scripts/Stations/Workstation.cs
@@ -6,18 +6,55 @@ using UnityEngine;
 
 public class Workstation : MonoBehaviour
 {
+    [Header("Station Stats")]
     [Tooltip("Determines the type of workstation")]
     public StationTypes type;
+    [Tooltip("Number of ticks it takes to process an ingredient")]
+    public int processTime;
 
     [Header("Unity Setup")]
     public StationSprites stationSprites;
+
+    [Header("Testing")]
+    [Tooltip("Used for testing while tick system isn't implemented. Set to -1 to disable fake ticks.")]
+    public float testTickTime;
     
+    // the child object that determines where the item will be 'held'
+    private Transform itemPos;
     private SpriteRenderer spriteRenderer;
     private int typeCode;
+    private int tickTimer;
+
+    public bool HasItem{
+        get{ return itemPos.childCount == 1; }
+    }
+
+    public bool IsProcessing{
+        get{ return tickTimer != 0; }
+    }
 
     private void Awake() {
+        tickTimer = 0;
         spriteRenderer = GetComponent<SpriteRenderer>();
         SetStation(type);
+    }
+
+    private void Start() {
+        // check if we should do test cycling
+        if (testTickTime < 0)
+        {
+            return;
+        }
+
+        StartCoroutine("TestSpawnCycle");
+    }
+
+    public void Tick(){
+        tickTimer--;
+    }
+
+    public void StartProcessing(Item i){
+
     }
 
     public void SetStation(StationTypes t)

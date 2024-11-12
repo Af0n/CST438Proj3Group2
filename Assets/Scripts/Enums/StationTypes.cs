d37dc6f9f1848998ab07f56133a1622fa31bc1e3
afonc <37196107+Af0n@users.noreply.github.com>
2024-11-11 21:32:04 -0800

    Prefab setup.
    - starting work on station functionality

---
+++ Assets/Scripts/Enums/StationTypes.cs
@@ -0,0 +1,5 @@
+public enum StationTypes{
+    GRINDSTONE = 0,
+    MIXING = 1,
+    CLEANSING = 2
+}
--- Assets/Scripts/Stations/Workstation.cs
+++ Assets/Scripts/Stations/Workstation.cs
@@ -6,9 +6,38 @@ using UnityEngine;
 
 public class Workstation : MonoBehaviour
 {
+    [Tooltip("Determines the type of workstation")]
+    public StationTypes type;
+
+    [Header("Unity Setup")]
+    public StationSprites stationSprites;
+    
     private SpriteRenderer spriteRenderer;
+    private int typeCode;
 
     private void Awake() {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        SetStation(type);
+    }
+
+    public void SetStation(StationTypes t)
+    {
+        typeCode = GetStationTypeCode(t);
+        spriteRenderer.sprite = stationSprites.sprites[typeCode];
+    }
+
+    // gets the numerical value for the type of station
+    public static int GetStationTypeCode(StationTypes t){
+        switch(t){
+            case StationTypes.GRINDSTONE:
+                return 0;
+            case StationTypes.MIXING:
+                return 1;
+            case StationTypes.CLEANSING:
+                return 2;
+            default:
+                Debug.Log("Could Not Find Type");
+                return -1;
+        }
     }
 }

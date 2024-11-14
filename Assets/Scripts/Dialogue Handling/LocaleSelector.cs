41a7d945d6c4185619ba0c97a0e60914dbf1da4e
Edward Leon <edleon761@gmail.com>
2024-11-13 20:08:35 -0800

    Starting the next video tutorial

--- Assets/Scripts/Dialogue Handling/LocaleSelector.cs
+++ Assets/Scripts/Dialogue Handling/LocaleSelector.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.Localization.Settings;
+
 public class LocaleSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    // bool that keeps track of whether the locale is currently getting changed
+    private bool active = false;
+
+    // can be called by other systems to changde the locale
+    public void ChangeLocale(int localeID)
     {
-        
+
+        if (active)
+        {
+            return;
+        }
+
+        StartCoroutine(SetLocale(localeID));
     }
 
-    // Update is called once per frame
-    void Update()
+    // Main function the switches the locale
+    IEnumerator SetLocale (int _localeID)
     {
-        
+
+        // Sets the tracker to true
+        active = true;
+
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[_localeID];
+
+        // and once the locale has been set, it will turn to false
+        active = false;
+
     }
+
 }

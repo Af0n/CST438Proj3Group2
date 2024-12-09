a44ee35c394e28cc22ea6e38806d1d8e5bb9f9ee
Alexander Betancourt <AlexanderBBetancourt@proton.me>
2024-12-09 06:48:16 -0800

    BlendRules

--- Assets/Scripts/ScriptableObjects/Tile.cs
+++ Assets/Scripts/ScriptableObjects/Tile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -5,6 +6,8 @@ using UnityEngine.Tilemaps;
 public class Tile : ScriptableObject
 {
     [Header("Tile object")]
+    [Tooltip("ID of the tile")]
+    public int TileID = 0; 
     [Tooltip("Sprites or models representing the tile")]
     public TileBase tileSprite;
     [Tooltip("Possible adjacent tiles for each direction (Up, Right, Down, Left)")]
@@ -15,4 +18,6 @@ public class Tile : ScriptableObject
     [Range(0,10)]
     [Tooltip("The multplier for Adjacencents tiles of the same type.")]
     public float adjacencyMultplier;
+    [Tooltip("Scriptable Object that holds AjacencyBlendingRules")]
+    public List<TileAdj> AdjacencyBlendRules;
 }
